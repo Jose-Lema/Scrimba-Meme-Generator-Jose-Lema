b611d40d06a943f50c0606e6618bd215ff3f5591
Jose-Lema <jose.lema.2310@gmail.com>
2024-03-23 11:44:42 -0700

    Fixed Header and got random meme url

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,9 +1,20 @@
+import memesData from "../memesData";
+
 export default function Meme(){
+
+    let url;
+    function getMemeImage(){
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        url = memesArray[randomNumber].url
+        console.log(url);
+        return url;
+    }
 
     //Remember to use the path 'src/...rest of path' to get images
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top Text"
@@ -14,8 +25,13 @@ export default function Meme(){
                     placeholder="Bottom Text"
                     className="form--input"
                 />
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
         </main>
     )
 }
