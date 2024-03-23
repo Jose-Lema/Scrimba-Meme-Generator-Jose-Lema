a2ee262625ff38d4a1728ae6211860513a8f324c
Jose-Lema <jose.lema.2310@gmail.com>
2024-03-23 11:13:43 -0700

    Created Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 export default function Header(){
     return(
-        <div>
-            
-        </div>
+        <header className="header">
+            {/* <img src="" alt="" /> */}
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
     )
 }
