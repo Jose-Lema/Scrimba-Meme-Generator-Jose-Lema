33ec5a48eee104ccc87e4601ace646cdec14e248
Jose Lema <jose.lema.2310@gmail.com>
2024-03-22 22:16:27 -0700

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Header from './components/Header'
+// import Meme from './components/Meme'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Meme.jsx
@@ -0,0 +1,9 @@
+export default function Meme(){
+
+    //Remember to use the path 'src/...rest of path' to get images
+    return(
+        <div>
+
+        </div>
+    )
+}
