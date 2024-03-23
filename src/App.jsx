a7c6a7f977c7a12a3fea3a98b0b3dcd5860316a1
Jose-Lema <jose.lema.2310@gmail.com>
2024-03-23 11:21:48 -0700

    Created basic Meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from './components/Header'
-// import Meme from './components/Meme'
+import Meme from './components/Meme'
 import './App.css'
 
 function App() {
   return (
     <div>
       <Header />
+      <Meme />
     </div>
   )
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,8 +2,20 @@ export default function Meme(){
 
     //Remember to use the path 'src/...rest of path' to get images
     return(
-        <div>
-
-        </div>
+        <main>
+            <form className="form">
+                <input 
+                    type="text"
+                    placeholder="Top Text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom Text"
+                    className="form--input"
+                />
+                <button className="form--button">Get a new meme image 🖼</button>
+            </form>
+        </main>
     )
 }
